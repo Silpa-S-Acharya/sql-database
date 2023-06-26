9293851cbbf8a186d98ea16484d7413bcfac65bb
Acharya Silpa <117834398+Silpa-S-Acharya@users.noreply.github.com>
2023-06-26 22:08:16 +0530

    Update Employee.sql

--- Employee.sql
+++ Employee.sql
@@ -2,6 +2,53 @@ mysql> create table employees(emp_id int primary key,fname varchar(10),lname var
 mysql> insert into regions values(1001,'Haryana'),(1002,'Sumatra'),(1003,'Jibla'),(1004,'Auckland'),(1005,'Chennai'),(1006,'Tianjin'),(1007,'Shanghai');
 Query OK, 7 rows affected (0.46 sec)
 Records: 7  Duplicates: 0  Warnings: 0
+   insert into employees values(68418,"Zack","Ryder","zackryder09@gmail.com",8754123362,"1999-08-09",777,20000,152,2),(67521,"Scarlet","Taylor","taylor34@gmail.com",9874562315,"2000-07-12",555,38000,101,1),(69532,"Kelly","John","kjohn18@gmail.com",7568412366,"1994-05-08",666,9500,102,3),(61234,"Randy","Orton","rkorton23@gmail.com",9632587410,"2002-05-11",333,55000,109,10);
+Query OK, 4 rows affected (0.18 sec)
+Records: 4  Duplicates: 0  Warnings: 0
+  insert into dependents values(512,"Luca","Reo","Brother-in-law",61234),(617,"Ricardo","Rodrigus"),(418,"Alberto","Rio","Son",68418),(331,"Natalya","Tyson");
+ insert into departments values(1,"Public Relations",1800),(2,"Human Resources",1400),(3,"Sales",1800);
+mysql> insert into locations values(1800,"Outer Ring Road",400008,"Nagapada","Tamil Nadu",204),(1400,"Dalian",116013,"Shahekou","Liaoning",205);
+mysql> insert into departments values(10,"Administration",1700),(20,"Marketing",1800),(30,"Purchasing",1700),(50,"Shipping",1500),(60,"IT",1400),(90,"Executive",1700);
+
+
+mysql> select * from jobs;
++-------+-------------------+--------+
+| jobid | job_title         | minsal |
++-------+-------------------+--------+
+|   111 | Manager           |  58000 |
+|   222 | Asst.Manager      |  35000 |
+|   333 | Analyst           |  43000 |
+|   444 | Trainer           |  38000 |
+|   555 | Mangaing Director |  68000 |
+|   666 | Accountant        |  78000 |
+|   777 | Supporter         |  28000 |
++-------+-------------------+--------+
+7 rows in set (0.02 sec)
+
+mysql> select * from locations;
++-------+-----------------+--------+-------------+------------+------+
+| locid | saddress        | pcode  | city        | sprovince  | cid  |
++-------+-----------------+--------+-------------+------------+------+
+|  1300 | New Bangaru     | 600078 | K K Nagar   | Tamil Nadu |  204 |
+|  1400 | Dalian          | 116013 | Shahekou    | Liaoning   |  205 |
+|  1500 | Nanjing         |  61105 | North China | Tianjin    |  206 |
+|  1700 | Crescent        |   1050 | Remuera     | Auckland   |  203 |
+|  1800 | Outer Ring Road | 400008 | Nagapada    | Tamil Nadu |  204 |
+|  1900 | Gorakhpur       | 122001 | Gurugram    | Chandigarh |  207 |
++-------+-----------------+--------+-------------+------------+------+
+6 rows in set (0.00 sec)
+
+mysql> select * from employees;
++--------+-----------+--------+----------------------------+------------+------------+-------+--------+------------+--------+
+| emp_id | fname     | lname  | email                      | phno       | hire_date  | jobid | salary | manager_id | deptid |
++--------+-----------+--------+----------------------------+------------+------------+-------+--------+------------+--------+
+|  61234 | Randy     | Orton  | rkorton23@gmail.com        | 9632587410 | 2002-05-11 |   333 |  55000 |        109 |     10 |
+|  67521 | Scarlet   | Taylor | taylor34@gmail.com         | 9874562315 | 2000-07-12 |   555 |  38000 |        101 |      1 |
+|  68319 | Charlotte | Flair  | charlotteflair09@gmail.com | 9784562231 | 2004-09-12 |   222 |  33000 |        147 |     50 |
+|  68418 | Zack      | Ryder  | zackryder09@gmail.com      | 8754123362 | 1999-08-09 |   777 |  20000 |        152 |      2 |
+|  69532 | Kelly     | John   | kjohn18@gmail.com          | 7568412366 | 1994-05-08 |   666 |   9500 |        102 |      3 |
++--------+-----------+--------+----------------------------+------------+------------+-------+--------+------------+--------+
+5 rows in set (0.00 sec)
 
 mysql> select * from regions;
 +----------+----------+
@@ -15,90 +62,9 @@ mysql> select * from regions;
 |     1006 | Tianjin  |
 |     1007 | Shanghai |
 +----------+----------+
-7 rows in set (0.00 sec)
-mysql> insert into countries values(2001,'India',1001),(2002,'India',1002),(2003,'New Zealand',1003),(2004,'New Zealand',1004),(2005,'India',1005
-Query OK, 7 rows affected (0.06 sec)
-Records: 7  Duplicates: 0  Warnings: 0
+7 rows in set (0.02 sec)
 
 mysql> select * from countries;
-+------------+--------------+-----------+
-| country_id | country_name | region_id |
-+------------+--------------+-----------+
-|       2001 | India        |      1001 |
-|       2002 | India        |      1002 |
-|       2003 | New Zealand  |      1003 |
-|       2004 | New Zealand  |      1004 |
-|       2005 | India        |      1005 |
-|       2006 | India        |      1006 |
-|       2007 | New Zealand  |      1007 |
-+------------+--------------+-----------+
-7 rows in set (0.00 sec)
-mysql> insert into locations values(1100,'Iroli',45321,'Rabbada','Lucknow',2001),(1200,'Thekemala',698123,'Karamveli','Chennai',2002),(1300,'Sector 23',2137,'Clevedon','United Kingdom',2003),(1400,'Sector 22',2150,'Mangere','United Kingdom',2004),(1700,'Sector 17',2149,'Whitford','United Kingdom',2007);
-Query OK, 5 rows affected (0.14 sec)
-Records: 5  Duplicates: 0  Warnings: 0
-
-mysql> select * from locations;
-+-------------+----------------+-------------+-----------+----------------+------------+
-| location_id | street_address | postal_code | city      | state_province | country_id |
-+-------------+----------------+-------------+-----------+----------------+------------+
-|        1100 | Iroli          |       45321 | Rabbada   | Lucknow        |       2001 |
-|        1200 | Thekemala      |      698123 | Karamveli | Chennai        |       2002 |
-|        1300 | Sector 23      |        2137 | Clevedon  | United Kingdom |       2003 |
-|        1400 | Sector 22      |        2150 | Mangere   | United Kingdom |       2004 |
-|        1700 | Sector 17      |        2149 | Whitford  | United Kingdom |       2007 |
-+-------------+----------------+-------------+-----------+----------------+------------+
-5 rows in set (0.00 sec)
-mysql> insert into departments values(1,'Finance',1200),(2,'Marketing',1400),(3,'Sales',1700),(4,'Quality',1300),(5,'Engineer',1100);Query OK, 5 rows affected (0.06 sec)
-Records: 5  Duplicates: 0  Warnings: 0
-
-mysql> select * from departments;
-+---------+-----------+-------------+
-| dept_id | dept_name | location_id |
-+---------+-----------+-------------+
-|       1 | Finance   |        1200 |
-|       2 | Marketing |        1400 |
-|       3 | Sales     |        1700 |
-|       4 | Quality   |        1300 |
-|       5 | Engineer  |        1100 |
-+---------+-----------+-------------+
-5 rows in set (0.00 sec)
-
-mysql> insert into jobs values(111,'Manager',60000,63000),(112,'AManager',45000,50000),(113,'HR',57000,60000),(114,'Security',28000,30000),(115,'SExecutive',29000,30000);
-Query OK, 5 rows affected (0.08 sec)
-Records: 5  Duplicates: 0  Warnings: 0
-mysql> select * from jobs;
-+--------+------------+--------+--------+
-| job_id | job_title  | minSal | maxSal |
-+--------+------------+--------+--------+
-|    111 | Manager    |  60000 |  63000 |
-|    112 | AManager   |  45000 |  50000 |
-|    113 | HR         |  57000 |  60000 |
-|    114 | Security   |  28000 |  30000 |
-|    115 | SExecutive |  29000 |  30000 |
-+--------+------------+--------+--------+
-5 rows in set (0.01 sec)
-| DEPARTMENT_ID | DEPARTMENT_NAME      | MANAGER_ID | LOCATION_ID |
-+---------------+----------------------+------------+-------------+
-|            10 | Administration       |        200 |        1700 |
-|            20 | Marketing            |        201 |        1800 |
-|            30 | Purchasing           |        114 |        1700 |
-|            40 | Human Resources      |        203 |        2400 |
-|            50 | Shipping             |        121 |        1500 |
-|            60 | IT                   |        103 |        1400 |
-|            70 | Public Relations     |        204 |        2700 |
-|            80 | Sales                |        145 |        2500 |
-|            90 | Executive            |        100 |        1700 |
-  emp_id | emp_name | job_name  | manager_id | hire_date  | salary  | commission | dep_id
---------+----------+-----------+------------+------------+---------+------------+--------
-  68319 | KAYLING  | PRESIDENT |            | 1991-11-18 | 6000.00 |            |   1001
-  66928 | BLAZE    | MANAGER   |      68319 | 1991-05-01 | 2750.00 |            |   3001
-  67832 | CLARE    | MANAGER   |      68319 | 1991-06-09 | 2550.00 |            |   1001
-  65646 | JONAS    | MANAGER   |      68319 | 1991-04-02 | 2957.00 |            |   2001
-  67858 | SCARLET  | ANALYST   |      65646 | 1997-04-19 | 3100.00 |            |   2001
-  insert into employees values(68319,"Charlotte","Flair","charlotteflair09@gmail.com",9784562231,"2004-09-12",222,33000,147,50);
-insert into jobs values(222,"Asst.Manager",35000),(333,"Analyst",43000),(444,"Trainer",38000),(555,"Mangaing Director",68000),(666,"Accountant",78000),(111,"Manager",58000),(777,"Supporter",28000);
- insert into departments values(10,"Administration",1700),(20,"Marketing",1800),(30,"Purchasing",1700),(50,"Shipping",1500),(60,"IT",1400),(90,"Executive",1700);
- insert into locations values(1800,"Outer Ring Road",400008,"Nagapada","Tamil Nadu",204),(1400,"Dalian",116013,"Shahekou","Liaoning",205);
 +-----+-----------+----------+
 | cid | cname     | regionid |
 +-----+-----------+----------+
@@ -110,34 +76,25 @@ insert into jobs values(222,"Asst.Manager",35000),(333,"Analyst",43000),(444,"Tr
 | 206 | China     |     1007 |
 | 207 | India     |     1001 |
 +-----+-----------+----------+
-7 rows in set (0.00 sec)
+7 rows in set (0.02 sec)
 
-mysql> select * from regions;
-+----------+----------+
-| regionid | rname    |
-+----------+----------+
-|     1001 | Haryana  |
-|     1002 | Sumatra  |
-|     1003 | Jibla    |
-|     1004 | Auckland |
-|     1005 | Chennai  |
-|     1006 | Tianjin  |
-|     1007 | Shanghai |
-+----------+----------+
-7 rows in set (0.04 sec)
+mysql> select * from departments;
++--------+------------------+-------+
+| deptid | depname          | locid |
++--------+------------------+-------+
+|      1 | Public Relations |  1800 |
+|      2 | Human Resources  |  1400 |
+|      3 | Sales            |  1800 |
+|     10 | Administration   |  1700 |
+|     20 | Marketing        |  1800 |
+|     30 | Purchasing       |  1700 |
+|     50 | Shipping         |  1500 |
+|     60 | IT               |  1400 |
+|     90 | Executive        |  1700 |
++--------+------------------+-------+
+9 rows in set (0.00 sec)
 
-mysql> select * from locations;
-+-------+-------------+--------+-------------+------------+------+
-| locid | saddress    | pcode  | city        | sprovince  | cid  |
-+-------+-------------+--------+-------------+------------+------+
-|  1300 | New Bangaru | 600078 | K K Nagar   | Tamil Nadu |  204 |
-|  1500 | Nanjing     |  61105 | North China | Tianjin    |  206 |
-|  1700 | Crescent    |   1050 | Remuera     | Auckland   |  203 |
-|  1900 | Gorakhpur   | 122001 | Gurugram    | Chandigarh |  207 |
-+-------+-------------+--------+-------------+------------+------+
-1.SELECT fname,lname
-  FROM employees 
-   WHERE locid = 1700;
+1.SELECT fname,lname FROM employees WHERE locid = 1700;
 2. SELECT fname,lname
   FROM employees 
    WHERE locid not in 1700;
